285d79a0032a78fd18d1174e1623dad4a3a9580f
Arnaldo <arnaldopedroquinones@gmail.com>
2025-01-05 23:55:21 -0300

    actualizando proyects

--- src/pages/main/proyects.py
+++ src/pages/main/proyects.py
@@ -1,12 +1,13 @@
 import reflex as rx
 from rxconfig import config
-from .modulos import sidebar_bottom_profile, pop_up_message
+from .modulos import header,sidebar_bottom_profile, pop_up_message
 
 
 def proyects() -> rx.Component:
     """Página Proyects."""
     return rx.box(
         rx.hstack(
+            header(),
             sidebar_bottom_profile(),  # Barra lateral
             rx.container(
                 rx.vstack(
