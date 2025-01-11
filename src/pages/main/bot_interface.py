6b3434e671962a39351b5d6b094fab5649d682e6
Arnaldo <arnaldopedroquinones@gmail.com>
2025-01-11 19:50:48 -0300

    actualizar imagen de fondo y agregar funcionalidad de chatbot

--- src/pages/main/bot_interface.py
+++ src/pages/main/bot_interface.py
@@ -4,8 +4,22 @@ import reflex as rx
 def stackbot() -> rx.Component:
     """Crea un componente personalizado con un contenedor dentro de un stack."""
     return rx.stack(
-              rx.container(rx.text("Chatbot", size="6", color="white"),
-            width="250px",
+        rx.flex(
+            rx.input(
+                rx.input.slot(
+                    rx.icon(tag="search"),
+                ),
+                placeholder="Ingrese consulta",
+                border_radius="40px",
+            ),
+            direction="column",
+            spacing="3",
+            style={"maxWidth": 500},
+            
+        ),
+        rx.container(
+            
+            width="270px",
             height="70vh",
             center_content=True,
             bg=rx.color("accent", 3),
@@ -15,4 +29,21 @@ def stackbot() -> rx.Component:
         position="absolute",  # Posicionamiento absoluto
         bottom="2%",  # Alineado al borde inferior
         right="10%",  # Alineado al borde izquierdo
+    )
+
+
+def chatbot_styles() -> rx.Component:
+    return rx.style(
+        """
+        @keyframes slideInFromBottom {
+            from {
+                transform: translateY(100%);
+                opacity: 0;
+            }
+            to {
+                transform: translateY(0);
+                opacity: 1;
+            }
+        }
+        """
     )
