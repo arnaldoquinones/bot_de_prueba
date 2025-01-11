401f2cfb86619666b757d47cedc5124da63d00a1
Arnaldo <arnaldopedroquinones@gmail.com>
2025-01-11 19:02:45 -0300

    se inicia la construccion de la interfase del bot en proyects

---
+++ src/pages/main/bot_interface.py
@@ -0,0 +1,19 @@
+import reflex as rx
+
+
+def stackbot() -> rx.Component:
+    """Crea un componente personalizado con un contenedor dentro de un stack."""
+    return rx.stack(
+        rx.text("Chatbot", size="6", color="white"),
+        rx.container(
+            width="200px",
+            height="75vh",
+            center_content=True,
+            bg=rx.color("accent", 3),
+            border_radius="15px",
+            box_shadow="10px 10px 15px rgba(0, 0, 0, 0.3), 0px 0px 5px transparent",
+        ),
+        position="absolute",  # Posicionamiento absoluto
+        bottom="2%",  # Alineado al borde inferior
+        right="10%",  # Alineado al borde izquierdo
+    )
--- src/pages/main/proyects.py
+++ src/pages/main/proyects.py
@@ -1,6 +1,7 @@
 import reflex as rx
 from rxconfig import config
 from .modulos import header,sidebar_bottom_profile, pop_up_message
+from .bot_interface import stackbot
 
 
 
@@ -31,7 +32,7 @@ def proyects() -> rx.Component:
                 width="80%",  # Ajusta el ancho del contenedor
                 padding="2rem",  # Ajusta el padding interno
             ),
-
+            stackbot(),
             sidebar_bottom_profile(),
             pop_up_message(),  # login_multiple_thirdparty() puede reemplazar esto si es necesario
             spacing="4",  # Ajusta el espaciado entre el contenido
