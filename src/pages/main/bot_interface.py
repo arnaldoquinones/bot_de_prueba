60762da999ed7f2b013db24bd1af7e7889b5ddc8
Arnaldo <arnaldopedroquinones@gmail.com>
2025-01-11 21:00:22 -0300

    progreso muy pobre de la interface

--- src/pages/main/bot_interface.py
+++ src/pages/main/bot_interface.py
@@ -5,31 +5,40 @@ def stackbot() -> rx.Component:
     """Crea un componente personalizado con un contenedor dentro de un stack."""
     return rx.stack(
         rx.flex(
-            rx.input(
-                rx.input.slot(
-                    rx.icon(tag="search"),
-                ),
-                placeholder="Ingrese consulta",
-                border_radius="40px",
-            ),
-            direction="column",
-            spacing="3",
-            style={"maxWidth": 500},
-            
-        ),
+    rx.input(
+        placeholder="Ingrese consulta",
+        border_radius="40px",
+        width="245px",
+    ),
+    rx.icon(
+        tag="send-horizontal",
+        margin_left="-40px", # Push icon to overlap with input
+        align_self="center", # Center vertically
+    ),
+    direction="row",
+    spacing="0", # Remove spacing between input and icon
+    align="center", # Center items vertically
+    style={"maxWidth": 700},
+    position="relative",
+    top="375px",
+    left="255px",
+),
         rx.container(
-            
-            width="270px",
-            height="70vh",
-            center_content=True,
-            bg=rx.color("accent", 3),
-            border_radius="15px",
-            box_shadow="10px 10px 15px rgba(0, 0, 0, 0.3), 0px 0px 5px transparent",
-        ),
-        position="absolute",  # Posicionamiento absoluto
+    width="270px",
+    height="70vh",
+    center_content=True,
+    background="url('https://github.com/arnaldoquinones/bot_de_prueba/blob/master/src/pages/assets/fondo_chatbot.jpg?raw=true') center/cover",  # Corregido el uso de la URL y la propiedad
+    border_radius="15px",
+    box_shadow="10px 10px 15px rgba(0, 0, 0, 0.3), 0px 0px 5px transparent",
+),
+
+        position="absolute",  # Posicionamiento absoluto para el contenedor principal
         bottom="2%",  # Alineado al borde inferior
         right="10%",  # Alineado al borde izquierdo
     )
+
+
+
 
 
 def chatbot_styles() -> rx.Component:
