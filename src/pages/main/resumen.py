6e2b9146669b572067bc5162710feabd837a304f
Arnaldo <arnaldopedroquinones@gmail.com>
2025-01-23 23:32:02 -0300

    nueva actualizacion para atras

--- src/pages/main/resumen.py
+++ src/pages/main/resumen.py
@@ -5,6 +5,9 @@ from .bot_interface import stackbot
 from whoosh.qparser import QueryParser
 from .modulos import ix  # Importa el índice desde modulos.py
 
+
+
+
 # Estado de la página resumen
 class ResumenState(rx.State):
     search_results: list[str] = []  # Almacenar resultados
@@ -27,6 +30,8 @@ def resumen_page() -> rx.Component:
         padding="2em",
     )
 
+
+# Página resumen
 def resumen() -> rx.Component:
     """Página Resumen."""
     return rx.box(
@@ -38,17 +43,28 @@ def resumen() -> rx.Component:
                         rx.fragment(
                             rx.text("Resumen de la búsqueda"),
                         ),
-                        size="6"
+                        size="6",
                     ),
                     resumen_page(),
-                    rx.image(
+                     rx.image(
                         src="https://github.com/arnaldoquinones/bot_de_prueba/blob/master/src/pages/assets/logo_fondo_transparente.png?raw=true",
                         width="120px",
                         height="auto",
                         border_radius="50%",
                         alt="Foto de perfil",
-                        margin_top="-58px",
-                        margin_left="-11.9em",
+                        margin_top="-177px",
+                        margin_left="-12.15em",
+                        animation="neonGlow 2s infinite alternate cubic-bezier(0.455, 0.030, 0.515, 0.955)",
+                        style={
+                            "@keyframes neonGlow": {
+                                "0%": {
+                                    "filter": "drop-shadow(0 0 10px rgba(255,255,255,.3)) drop-shadow(0 0 20px rgba(255,255,255,.3)) drop-shadow(0 0 40px rgba(66,220,219,.3)) drop-shadow(0 0 60px rgba(66,220,219,.3))"
+                                },
+                                "100%": {
+                                    "filter": "drop-shadow(0 0 2px rgba(255,255,255,.2)) drop-shadow(0 0 8px rgba(255,255,255,.2)) drop-shadow(0 0 20px rgba(66,220,219,.2)) drop-shadow(0 0 30px rgba(66,220,219,.2))"
+                                }
+                            }
+                        }
                     ),
                     spacing="4",  # Usa un valor numérico entre 0 y 9
                 ),
